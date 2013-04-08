c5f6d1778de58c8a04233c3d3577bc33da754d42
KAMEDAkyosuke <kameda-kyosuke@kayac.com>
2013-04-08 15:04:56 +0900

    add HelloTriangle

---
+++ iOS/HelloTriangle/HelloTriangle/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  HelloTriangle
+//
+//  Created by organlounge on 2013/04/06.
+//  Copyright (c) 2013年 KAMEDAkyosuke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ iOS/HelloTriangle/HelloTriangle/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  HelloTriangle
+//
+//  Created by organlounge on 2013/04/06.
+//  Copyright (c) 2013年 KAMEDAkyosuke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
